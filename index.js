82388a84cc62c50928f8ea330404a1e8c8122116
saurabhK99 <saurabhkrjha98@gmail.com>
2022-06-13 20:48:08 +0530

    finalized the project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import child_process from 'child_process'
 import path from 'path'
 import dotenv from 'dotenv'
+import { readSync } from 'fs'
 
 dotenv.config()
 const app = express()
@@ -9,8 +10,18 @@ const execFile = child_process.execFile
 app.use(express.static(path.join(path.resolve(), 'public')))
 
 app.get('/simulation', (req, res) => {
-    execFile(path.resolve(path.resolve(), 'a.out'))
-    console.log(path.resolve(path.resolve(), 'a.out'))
+    execFile(path.resolve(path.resolve(), 'a.out'), [], (error, stdout, stderr) => {
+        if(error) {
+            res.send(error)
+            return
+        }
+        if(stderr) {
+            res.send(stderr)
+            return
+        }
+
+        res.send(stdout)
+    })
 })
 
 app.get('*', (req, res) => {
